62a0e7de0c13bdd08366a3a48b1a78789249ff98
Jeffry-1975 <jefferson-paredes@hotmail.com>
2026-04-22 18:04:52 -0500

    Version_1.0

---
+++ config.js
@@ -0,0 +1,2 @@
+export const supabaseUrl = 'https://crtyrcabkowrfrwcyidx.supabase.co';
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNydHlyY2Fia293cmZyd2N5aWR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNTY2OTksImV4cCI6MjA5MTkzMjY5OX0.Vl-aZTTugDXn_mYw8_eUkSGA25K0x_SYlSHGXzjxzTQ';
